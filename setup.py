650036f187238ac9cae648f15e1b228bc26f0772
Joseph D. Marhee <jmarhee@interiorae.com>
2020-09-15 05:02:07 -0500

    Updates to reflect new git repo

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 setup(name='pyputio',
-version='0.1.5',
+version='0.1.6',
 description='Command Line Client for Put.io download zip archives',
-url='https://github.com/jmarhee/pyputio-cli',
+url='https://git-central.openfunction.co/jmarhee/pyputio-cli',
 author='jmarhee',
 author_email='jmarhee@interiorae.com',
 license='MIT',
