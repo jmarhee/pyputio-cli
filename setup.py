1976a43b642b16216ea1077a2d66eefee20fd53c
Joseph D. Marhee <jmarhee@interiorae.com>
2020-10-22 01:25:12 -0500

    Bumps release, updates progress reporter and completion report

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 setup(name='pyputio',
-version='0.1.10',
+version='0.1.11',
 description='Command Line Client for Put.io download zip archives',
 url='https://git-central.openfunction.co/jmarhee/pyputio-cli',
 author='jmarhee',
 author_email='jmarhee@interiorae.com',
 license='MIT',
 packages=['pyputio'],
-python_requires='>=3',
+python_requires='>=3.8',
 entry_points = {
 	'console_scripts': ['putio=pyputio.main:main']
 },
