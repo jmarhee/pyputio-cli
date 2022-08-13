fe45a60495d6d9881e2cedb549a1c1ec22a9c970
Joseph Marhee <jmarhee@interiorae.com>
2022-08-13 15:39:27 -0500

    Removes plex auth logic from scan script, and updates main to reflect the new plex script
    Signed-off-by: Joseph Marhee <jmarhee@interiorae.com>

--- pyputio/scan.py
+++ pyputio/scan.py
@@ -1,22 +1,12 @@
 from plexapi.myplex import MyPlexAccount
 import os
 import requests
-
-def plexAuth():
-    plex_auth_info = {
-        "username": os.environ['PLEX_USERNAME'],
-        "password": os.environ['PLEX_PASSWORD'],
-        "server": os.environ['PLEX_SERVER_NAME']
-    }
-
-    account = MyPlexAccount(plex_auth_info['username'], plex_auth_info['password'])
-    plex = account.resource(plex_auth_info['server']).connect()
-    return plex
+from pyputio.plex import plexAuth
 
 def readPlexCollections():
-	plex = plexAuth()
-	collections = plex.library.sections()
-	return collections
+    plex = plexAuth()
+    collections = plex.library.sections()
+    return collections
 
 def plexUpdate():
 	plex = plexAuth()
@@ -27,6 +17,8 @@ def plexUpdate():
 def update(plex):
     collections = plex.library.sections()
     task = plex.library.update()
+    if os.environ.get('PUTIO_PLEX_METADATA_REFRESH_HARD') is not None:
+        plex.library.refresh()
     return task
 
 def notify(message):
